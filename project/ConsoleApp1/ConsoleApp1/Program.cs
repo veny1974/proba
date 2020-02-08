7e916c9400f7616999ac2249bc21e3e650f79e0a
venyton4 <veny_t@abv.bg>
2020-02-08 11:08:06 +0200

    new

--- project/ConsoleApp1/ConsoleApp1/Program.cs
+++ project/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var a = Console.ReadLine();
+
             Console.WriteLine("Hello World!");
         }
     }
